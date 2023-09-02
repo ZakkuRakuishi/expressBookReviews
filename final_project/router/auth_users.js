5241ebf43562cb68d89e299da6ec5212c3e39d5d
ZakkuRakuishi <37822758+ZakkuRakuishi@users.noreply.github.com>
2023-09-02 15:15:59 -0700

    Had to change manually as Theia doesn't let me login

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,30 +1,69 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-let books = require("./booksdb.js");
-const regd_users = express.Router();
 
-let users = [];
+const regd_users = express.Router(); 
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+let users = [
+  {username: 'johnDoe', password: '123456'}
+];
+
+const authenticatedUser = (username, password) => {
+  return users.find(u => u.username === username && u.password === password);
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
-}
+// Login route 
+regd_users.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-//only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if(!username || !password) {
+    return res.status(400).json({message: 'Username and password required'});
+  }
+
+  const user = authenticatedUser(username, password);
+
+  if(!user) {
+    return res.status(401).json({message: 'Invalid credentials'});
+  }
+
+  const token = jwt.sign({sub: user.username}, 'secretkey');
+
+  req.session.user = {
+    username: user.username,
+    token    
+  }
+
+  return res.json({message: 'Logged in successfully!'});
+
 });
 
-// Add a book review
+// Add/update review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  const reviewText = req.query.review;
+  const username = req.session.user.username;
+
+  // Find book and update review
+  books[isbn].reviews.push({
+    user: username,
+    text: reviewText 
+  });
+
+  res.json({message: 'Review added successfully!'});
+
 });
 
-module.exports.authenticated = regd_users;
-module.exports.isValid = isValid;
-module.exports.users = users;
+
+// Delete review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const username = req.session.user.username;
+
+  books[isbn].reviews = books[isbn].reviews.filter(r => r.user !== username);
+
+  res.json({message: 'Review deleted successfully!'});
+});
+
+module.exports = {
+  authenticated: regd_users
+};
