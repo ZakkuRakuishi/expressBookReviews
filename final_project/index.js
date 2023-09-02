9948e3604a905fa463c66a09794d91649ff1af4a
ZakkuRakuishi <37822758+ZakkuRakuishi@users.noreply.github.com>
2023-09-02 15:16:25 -0700

    Had to change manually as Theia doesn't let me login

--- final_project/index.js
+++ final_project/index.js
@@ -1,22 +1,77 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const session = require('express-session');
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
+
+let users = []; // Assuming the user storage for registration and login
+
+const doesExist = (username) => {
+  return users.some(user => user.username === username);
+};
+
+const authenticatedUser = (username, password) => {
+  return users.some(user => user.username === username && user.password === password);
+};
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use(session({
+    secret: "fingerprint_customer",
+    resave: true,
+    saveUninitialized: true
+}));
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session.authorization) {
+        const token = req.session.authorization['accessToken'];
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            } else {
+                return res.status(403).json({ message: "User not authenticated" });
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" });
+    }
 });
- 
-const PORT =5000;
+
+app.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({ "username": username, "password": password });
+            return res.status(200).json({ message: "User successfully registered. Now you can login" });
+        } else {
+            return res.status(404).json({ message: "User already exists!" });
+        }
+    }
+    return res.status(404).json({ message: "Unable to register user." });
+});
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    if (authenticatedUser(username, password)) {
+        const accessToken = jwt.sign({ data: password }, 'access', { expiresIn: 60 * 60 });
+        req.session.authorization = { accessToken, username };
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+});
+
+const PORT = 5000;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
